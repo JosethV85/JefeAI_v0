310351c0a2f22597d22533448a7383912869e98e
JosethV85 <jvaldezl@isise.edu.pe>
2026-09-02 18:30:48 -0500

    Version JefeAI_v0.1

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
+def analizar_mensaje(mensaje):
+
+    mensaje = mensaje.lower()
+
+    saludos = [
+        "hola",
+        "buenas",
+        "hey",
+        "holi",
+        "qué tal",
+        "que tal",
+        "buenos días",
+        "buenas tardes",
+        "buenas noches"
+    ]
+
+    for saludo in saludos:
+        if saludo in mensaje:
+            return "saludo"
+
+    return "desconocido"
+
+
+def responder(intencion):
+
+    if intencion == "saludo":
+        return "Hola jefe 😎"
+
+    return "Todavía no sé responder eso."
+
+
 print("================================")
-print("          JEFEAI v0.1")
+print("          JEFEAI v0.2")
 print("================================")
 
 while True:
 
     mensaje = input("Tú > ")
 
-    if mensaje.lower() == "hola":
-        print("JefeAI > Hola jefe 😎")
-
-    elif mensaje.lower() == "como estas":
-        print("JefeAI > Todo bien, jefe.")
-
-    elif mensaje.lower() == "adios":
+    if mensaje.lower() == "adios":
         print("JefeAI > Nos vemos, jefe.")
         break
 
-    else:
-        print("JefeAI > Todavía no sé responder eso.")
+    intencion = analizar_mensaje(mensaje)
+
+    respuesta = responder(intencion)
+
+    print("JefeAI >", respuesta)
